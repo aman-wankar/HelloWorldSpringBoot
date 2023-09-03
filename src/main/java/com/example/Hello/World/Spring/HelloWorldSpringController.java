9b43c74d1cee3dfbe03feebf707a14d0f84a9bf0
809902 <amanwankar07@gmail.com>
2023-09-04 00:07:55 +0530

    Used Params in Request.

--- src/main/java/com/example/Hello/World/Spring/HelloWorldSpringController.java
+++ src/main/java/com/example/Hello/World/Spring/HelloWorldSpringController.java
@@ -1,17 +1,29 @@
 package com.example.Hello.World.Spring;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldSpringController {
     @RequestMapping("/")
     public String helloWorld() {
-        return "Hello World from Spring Boot";
+        return "Hello World from Spring Boot !";
     }
 
     @RequestMapping("/goodbye")
     public String goodbye() {
-        return "Goodbye from Spring Boot";
+        return "Goodbye from Spring Boot !";
+    }
+
+    @RequestMapping("/id/{id}")
+    public String getId(@PathVariable Integer id) {
+        return "The given ID is: " + id + ".";
+    }
+
+    @RequestMapping("/greet/{name}")
+    public String getId(@PathVariable String name) {
+        return "Hello " + name + ".";
     }
 }
